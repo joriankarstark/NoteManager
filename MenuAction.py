598ba3d18cd120ccdc5a700b57967f371973ebdc
jorian <jorian@gmail.com>
2016-09-04 18:37:04 -0500

    initial commit

---
+++ MenuAction.py
@@ -0,0 +1,12 @@
+
+class MenuAction:
+    """Action to attach to menu item"""
+
+    action_count = 0;
+
+    def __init__(self):
+        action_count++
+
+
+
+
---
+++ MenuItem.py
@@ -0,0 +1,10 @@
+
+class MenuItem:
+    """Element of the main menu"""
+
+    def __init__(self, title, action):
+        self.title = title
+        self.action = action
+        self.shortcut = []
+
+
